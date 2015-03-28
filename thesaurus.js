2684221bacf6e6a43f8dd02af14992eb1d6058aa
alpha-zero <alpha-zero@live.com>
2015-03-28 13:12:48 +1300

    minor locations update

--- thesaurus.js
+++ thesaurus.js
@@ -91,8 +91,12 @@ var cityinfo = {
 		"features" : [],
 	},
 	
-	// Minor Cities & Features
+// ## Minor Cities & Features sorted by region 
 
+	// CRAWLING KINGDOM
+	
+		//Waverwen
+	
 	"turning" : {
 		"id" : 8,
 		"type" : "Minor",
@@ -101,6 +105,301 @@ var cityinfo = {
 		"region" : "Crawling Kingdom",
 		"district" : "Waverwen",
 		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"lugia" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Lugia",
+		"region" : "Crawling Kingdom",
+		"district" : "Waverwen",
+		"description" : "Most of the the treasuring Waverwen holds is transported here.",
+		"majorpop" : "",
+		"features" : ["Main Bank of Waverwen"],
+	},
+	"waverlycoast" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Waverly Coast Township",
+		"region" : "Crawling Kingdom",
+		"district" : "Waverwen",
+		"description" : "The harbor village of Waverly Coast is important for the economy of Waverwen as this township delivers most of the food resources, catching game from nearly areas and using the nearby waters for fishing.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"coaltown" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Coaltown",
+		"region" : "Crawling Kingdom",
+		"district" : "Waverwen",
+		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+		// Yiva
+	
+	"trasten" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Trasten Town",
+		"region" : "Crawling Kingdom",
+		"district" : "Yiva",
+		"description" : "A small village town on the eastern coast of Yiva.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+		// Nothern Crawl
+	
+	"mogbalthur" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Mog Balthur",
+		"region" : "Crawling Kingdom",
+		"district" : "Nothern Crawl",
+		"description" : "The fortress in the mountains, the youngest of the 3 Esmorzar Dwarven cities.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"yestir" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Yestir",
+		"region" : "Crawling Kingdom",
+		"district" : "Northern Crawl",
+		"description" : "Sitting in the mountains in the Northern Peaks, a village.",
+		"majorpop" : "",
+		"features" : ["Cartographers Guild tower"],
+	},
+
+	// Cape Drifting
+
+	"cactir" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Cactir",
+		"region" : "Cape Drifting",
+		"district" : "",
+		"description" : "In a snowy harbor in the Drifting Cape, Cactir a fishing town.",
+		"majorpop" : "",
+		"features" : [],
+	},
+
+	// Endling
+	
+		// Brocking	
+
+	"fallcrest" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Fallcrest",
+		"region" : "Endling",
+		"district" : "Brocking",
+		"description" : "A village sitting in the intersection in Brocking. It sits on the Kings Road, connecting multiple regions.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"grogon" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Grogon",
+		"region" : "Endling",
+		"district" : "Brocking",
+		"description" : "A village sitting on Brocking's Cape.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"caediumforks" : {
+		"id" : 8,
+		"type" : "Keyloc",
+		"coord" : [17.30869, -110.74219],
+		"name" : "The Caedium Forks",
+		"region" : "Endling",
+		"district" : "Brocking",
+		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+		// Godsend	
+	
+		// North End
+	
+		// Trevoy (The crossing)
+
+	"brova" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Nebelheim",
+		"region" : "Endling",
+		"district" : "Trevoy 'The Forks'",
+		"description" : "",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+	// LEYLANDS
+	
+		// Dweiwar (eastern wilds)
+	
+	"increst" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Turning Outpost Village",
+		"region" : "Leylands",
+		"district" : "Dweiwar",
+		"description" : "",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"kanteen" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Turning Outpost Village",
+		"region" : "Leylands",
+		"district" : "Dweiwar",
+		"description" : "",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+		// Kotia
+	
+	"rocton" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Rocton Outpost Village",
+		"region" : "Leylands",
+		"district" : "Kotia",
+		"description" : "Serves as a bridging village connecting the regions together, it sits at an intersection on the Kings road.",
+		"majorpop" : "",
+		"features" : ["Adventurers Guild"],
+	},
+	"illyasyl" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Illyasyl Outpost Village",
+		"region" : "Leylands",
+		"district" : "Kotia",
+		"description" : "An elven outpost village that borders the Mweiwar Wilds. This village serves as guard to the elven cities within Mweiwar.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"maeimadtower" : {
+		"id" : 8,
+		"type" : "Keyloc",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Maeimad Magicks Tower",
+		"region" : "Leylands",
+		"district" : "Kotia",
+		"description" : "",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+		//Myeiwar Wilds
+	
+	"elheim" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Elheim",
+		"region" : "Leylands",
+		"district" : "Myeiwar Wilds",
+		"description" : "One of the greater Elven townships in the Myeiwar Wilds. Elheim is known for its skilled hunters.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"nebelheim" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Nebelheim",
+		"region" : "Leylands",
+		"district" : "Myeiwar Wilds",
+		"description" : "A greater Elven township in the Myeiwar Wilds.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	
+	// SHATTERLANDS
+	
+		//Craggy
+	
+	"grenth" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Grenth",
+		"region" : "Shatterlands",
+		"district" : "Craggy",
+		"description" : "The Harbor township of Grenth has become known for its bandits and piracy. It's become a popular cove for thieves and the like to come and sell stolen goods.",
+		"majorpop" : "",
+		"features" : ["Black Market"],
+	},
+	"forendicastle" : {
+		"id" : 8,
+		"type" : "Keyloc",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Forendi Castle",
+		"region" : "Shatterlands",
+		"district" : "Craggy",
+		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen.",
+		"majorpop" : "",
+		"features" : [],
+	},
+		
+		//Karren
+	
+	"summerhall" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Summerhall",
+		"region" : "Shatterlands",
+		"district" : "Karren",
+		"description" : "On the North-Eastern corner of the Scorchedwastes is a torn down desert village known as Summerhall. It used to be a fortress castletown, but has since just become a regular small town for traders and merchants.",
+		"majorpop" : "",
+		"features" : ["Summerhall Castle"],
+	},
+		//Sharden
+	
+	"albanon" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Albanon",
+		"region" : "Shatterlands",
+		"district" : "Sharden",
+		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen.",
+		"majorpop" : "",
+		"features" : [],
+	},
+	"hureil" : {
+		"id" : 8,
+		"type" : "Minor",
+		"coord" : [17.30869, -110.74219],
+		"name" : "Hureil",
+		"region" : "Shatterlands",
+		"district" : "Sharden",
+		"description" : "The outpost township of turning is one of the major villages that acts as guards to the gates of Waverwen.",
 		"majorpop" : "",
 		"features" : [],
 	},
